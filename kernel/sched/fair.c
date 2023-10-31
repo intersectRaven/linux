3ac1e8b4a83528753dbd7ecbabaec9962022c7a2
Jan Alexander Steffens (heftig) <heftig@archlinux.org>
2023-10-31 19:03:10 +0100

    ZEN: INTERACTIVE: Tune EEVDF for interactivity
    5.7:
    Take "sysctl_sched_nr_migrate" tune from early XanMod builds of 128. As
    of 5.7, XanMod uses 256 but that may affect applications that require
    timely response to IRQs.
    
    5.15:
    Per [a comment][1] on our ZEN INTERACTIVE commit, reducing the cost of
    migration causes the system less responsive under high load.  Most
    likely the combination of reduced migration cost + the higher number of
    tasks that can be migrated at once contributes to this.
    
    To better handle this situation, restore the mainline migration cost
    value and also reduce the max number of tasks that can be migrated in
    batch from 128 to 64.
    
    If this doesn't help, we'll restore the reduced migration cost and keep
    total number of tasks that can be migrated at once to 32.
    
    [1]: https://github.com/zen-kernel/zen-kernel/commit/be5ba234ca0a5aabe74bfc7e1f636f085bd3823c#commitcomment-63159674
    
    6.6:
    Port the tuning to EEVDF, which removed a couple of settings.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -75,8 +75,8 @@ unsigned int sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_base_slice			= 750000ULL;
-static unsigned int normalized_sysctl_sched_base_slice	= 750000ULL;
+unsigned int sysctl_sched_base_slice			= 400000ULL;
+static unsigned int normalized_sysctl_sched_base_slice	= 400000ULL;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -135,7 +135,7 @@ int __weak arch_asym_cpu_priority(int cpu)
  *
  * (default: 5 msec, units: microseconds)
  */
-static unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
+static unsigned int sysctl_sched_cfs_bandwidth_slice		= 3000UL;
 #endif
 
 #ifdef CONFIG_NUMA_BALANCING
