09bd3d13339ca135f80d60043864f1cf367441ce
Jason A. Donenfeld <Jason@zx2c4.com>
2024-07-03 20:31:12 +0200

    arch: allocate vgetrandom_alloc() syscall number
    Add vgetrandom_alloc() as syscall 463 (or 573 on alpha) by adding it to
    all of the various syscall.tbl and unistd.h files.
    
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -929,6 +929,8 @@ __SYSCALL(__NR_lsm_get_self_attr, sys_lsm_get_self_attr)
 __SYSCALL(__NR_lsm_set_self_attr, sys_lsm_set_self_attr)
 #define __NR_lsm_list_modules 461
 __SYSCALL(__NR_lsm_list_modules, sys_lsm_list_modules)
+#define __NR_vgetrandom_alloc 462
+__SYSCALL(__NR_vgetrandom_alloc, sys_vgetrandom_alloc)
 
 /*
  * Please add new compat syscalls above this comment and update
