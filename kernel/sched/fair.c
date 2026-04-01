90d76b4a3741e3dd76c2ffde8538de1e218a0776
Chen Yu <yu.c.chen@intel.com>
2026-04-01 14:52:28 -0700

    sched/cache: Disable cache aware scheduling for processes with high thread counts
    A performance regression was observed by Prateek when running hackbench
    with many threads per process (high fd count). To avoid this, processes
    with a large number of active threads are excluded from cache-aware
    scheduling.
    
    With sched_cache enabled, record the number of active threads in each
    process during the periodic task_cache_work(). While iterating over
    CPUs, if the currently running task belongs to the same process as the
    task that launched task_cache_work(), increment the active thread count.
    
    If the number of active threads within the process exceeds the number
    of Cores(divided by SMTs number) in the LLC, do not enable cache-aware
    scheduling. However, on system with smaller number of CPUs within 1 LLC,
    like Power10/Power11 with SMT4 and LLC size of 4, this check effectively
    disables cache-aware scheduling for any process. One possible solution
    suggested by Peter is to use a LLC-mask instead of a single LLC value
    for preference. Once there are a 'few' LLCs as preference, this constraint
    becomes a little easier. It could be an enhancement in the future.
    
    For users who wish to perform task aggregation regardless, a debugfs knob
    is provided for tuning in a subsequent patch.
    
    Suggested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Suggested-by: Aaron Lu <ziqianlu@bytedance.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Co-developed-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1317,6 +1317,12 @@ static inline bool valid_llc_buf(struct sched_domain *sd,
 	return true;
 }
 
+static bool exceed_llc_nr(struct mm_struct *mm, int cpu)
+{
+	return !fits_capacity((mm->sc_stat.nr_running_avg * cpu_smt_num_threads),
+			per_cpu(sd_llc_size, cpu));
+}
+
 static void account_llc_enqueue(struct rq *rq, struct task_struct *p)
 {
 	struct sched_domain *sd;
@@ -1508,7 +1514,8 @@ void account_mm_sched(struct rq *rq, struct task_struct *p, s64 delta_exec)
 	 */
 	if (time_after(epoch,
 		       READ_ONCE(mm->sc_stat.epoch) + EPOCH_LLC_AFFINITY_TIMEOUT) ||
-	    get_nr_threads(p) <= 1) {
+	    get_nr_threads(p) <= 1 ||
+	    exceed_llc_nr(mm, cpu_of(rq))) {
 		if (mm->sc_stat.cpu != -1)
 			mm->sc_stat.cpu = -1;
 	}
@@ -1593,13 +1600,31 @@ out:
 	cpumask_copy(cpus, cpu_online_mask);
 }
 
+static inline void update_avg_scale(u64 *avg, u64 sample)
+{
+	int factor = per_cpu(sd_llc_size, raw_smp_processor_id());
+	s64 diff = sample - *avg;
+	u32 divisor;
+
+	/*
+	 * Scale the divisor based on the number of CPUs contained
+	 * in the LLC. This scaling ensures smaller LLC domains use
+	 * a smaller divisor to achieve more precise sensitivity to
+	 * changes in nr_running, while larger LLC domains are capped
+	 * at a maximum divisor of 8 which is the default smoothing
+	 * factor of EWMA in update_avg().
+	 */
+	divisor = clamp_t(u32, (factor >> 2), 2, 8);
+	*avg += div64_s64(diff, divisor);
+}
+
 static void task_cache_work(struct callback_head *work)
 {
-	struct task_struct *p = current;
+	struct task_struct *p = current, *cur;
+	int cpu, m_a_cpu = -1, nr_running = 0;
+	unsigned long curr_m_a_occ = 0;
 	struct mm_struct *mm = p->mm;
 	unsigned long m_a_occ = 0;
-	unsigned long curr_m_a_occ = 0;
-	int cpu, m_a_cpu = -1;
 	cpumask_var_t cpus;
 
 	WARN_ON_ONCE(work != &p->cache_work);
@@ -1608,6 +1633,13 @@ static void task_cache_work(struct callback_head *work)
 
 	if (p->flags & PF_EXITING)
 		return;
+
+	if (get_nr_threads(p) <= 1) {
+		if (mm->sc_stat.cpu != -1)
+			mm->sc_stat.cpu = -1;
+
+		return;
+	}
 
 	if (!zalloc_cpumask_var(&cpus, GFP_KERNEL))
 		return;
@@ -1631,6 +1663,12 @@ static void task_cache_work(struct callback_head *work)
 				if (occ > m_occ) {
 					m_occ = occ;
 					m_cpu = i;
+				}
+				scoped_guard (rcu) {
+					cur = rcu_dereference_all(cpu_rq(i)->curr);
+					if (cur && !(cur->flags & (PF_EXITING | PF_KTHREAD)) &&
+					    cur->mm == mm)
+						nr_running++;
 				}
 			}
 
@@ -1675,6 +1713,7 @@ static void task_cache_work(struct callback_head *work)
 		mm->sc_stat.cpu = m_a_cpu;
 	}
 
+	update_avg_scale(&mm->sc_stat.nr_running_avg, nr_running);
 	free_cpumask_var(cpus);
 }
 
@@ -10100,6 +10139,13 @@ static enum llc_mig can_migrate_llc_task(int src_cpu, int dst_cpu,
 	cpu = mm->sc_stat.cpu;
 	if (cpu < 0 || cpus_share_cache(src_cpu, dst_cpu))
 		return mig_unrestricted;
+
+	/* skip cache aware load balance for single/too many threads */
+	if (get_nr_threads(p) <= 1 || exceed_llc_nr(mm, dst_cpu)) {
+		if (mm->sc_stat.cpu != -1)
+			mm->sc_stat.cpu = -1;
+		return mig_unrestricted;
+	}
 
 	if (cpus_share_cache(dst_cpu, cpu))
 		to_pref = true;
